0e8ac1456a579b9eb005579ec46bdc5712eb1f3f
Andhitia Rama <andhitia.r@gmail.com>
2022-05-16 09:09:45 +0700

    [ssi_account_amortization] 14.0.1.0.0

---
+++ ssi_account_amortization/models/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from . import (
+    amortization_type,
+    account_amortization,
+    amortization_schedule,
+)
